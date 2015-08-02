3c57f34a7a3e78e11eb761b4562eb811d3b674b7
Vlad Shablinsky <vladshablinsky@gmail.com>
2015-08-03 01:06:32 +0300

    Add dotfiles

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# make symlinks
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="zshrc vimrc tmux.conf oh-my-zsh"
+
+echo "Creating $olddir for backup of any dotfiles in ~"
+mkdir -p $olddir
+echo "...completed"
+
+cd $dir
+for file in $files; do
+  mv ~/.$file ~/dotfiles_old/
+  ln -s $dir/$file ~/.$file
+done
+
+install_oh_my_zsh () {
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+  if [[ ! -d $dir/oh-my-zsh/ ]]; then
+      git clone http://github.com/robbyrussell/oh-my-zsh.git
+  fi
+
+  # if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+  #     chsh -s $(which zsh)
+  # fi
+  echo "You might want to change shell, use chsh -s $(which zsh)"
+else
+  platform=$(uname);
+
+  if [[ $platform == 'Linux' ]]; then
+    sudo apt-get install zsh
+    install_oh_my_zsh
+  elif [[ $platform == 'Darwin' ]]; then
+    if command -v brew >/dev/null 2>&1 ;then
+      brew install zsh
+      install_oh_my_zsh
+    else
+      echo "You got it without zsh"
+      exit
+    fi
+  fi
+fi
+}
+
+install_oh_my_zsh
