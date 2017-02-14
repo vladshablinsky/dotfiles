bfb824c53c10798acba7473bbf5c568a4a12213f
Uladzislau Shablinski <vladshablinsky@gmail.com>
2017-02-14 11:59:00 +0300

    install: handle edge cases better

--- install.sh
+++ install.sh
@@ -5,44 +5,39 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 files="zshrc vimrc vim gitconfig tmux.conf oh-my-zsh brew_scripts brew_livecheck_watchlist"
 
+if [[ -d $olddir ]]; then
+  echo "First cleanup your $olddir"
+  exit 1
+fi
+
 echo "Creating $olddir for backup of any dotfiles in ~"
 mkdir -p $olddir
-echo "...completed"
+echo "...completed!"
 
 cd $dir
 for file in $files; do
-  if [ -f $file ]; then
-    mv ~/.$file ~/dotfiles_old/
+  echo $file
+  if [ -f ~/.$file -o -d ~/.$file ]; then
+    mv ~/.$file $olddir && ln -s $dir/$file ~/.$file
+  else
+    ln -s $dir/$file ~/.$file
   fi
-  ln -s $dir/$file ~/.$file
 done
+echo "...completed!"
 
 install_oh_my_zsh () {
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
   if [[ ! -d $dir/oh-my-zsh/ ]]; then
-      git clone http://github.com/robbyrussell/oh-my-zsh.git
+    git clone http://github.com/robbyrussell/oh-my-zsh.git
   fi
 
-  # if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-  #     chsh -s $(which zsh)
-  # fi
-  echo "You might want to change shell, use chsh -s $(which zsh)"
-else
-  platform=$(uname);
-
-  if [[ $platform == 'Linux' ]]; then
-    sudo apt-get install zsh
-    install_oh_my_zsh
-  elif [[ $platform == 'Darwin' ]]; then
-    if command -v brew >/dev/null 2>&1 ;then
-      brew install zsh
-      install_oh_my_zsh
-    else
-      echo "You got it without zsh"
-      exit
+  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        echo "Changing shell to zsh..."
+        chsh -s $(which zsh)
     fi
+  else
+    echo "You might want to install zsh."
   fi
-fi
 }
 
 install_oh_my_zsh
